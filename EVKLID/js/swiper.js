6bd648cfb4e92f7a9fe5bbacef693547751a472e
saoncy <saooncy@gmail.com>
2023-02-22 14:17:38 +0200

    added swiper to the hero section

---
+++ EVKLID/js/swiper.js
@@ -0,0 +1,11 @@
+new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+  },
+});
