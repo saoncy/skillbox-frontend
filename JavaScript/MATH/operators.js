0f0a06e711795aff05476dbc764dba91f6cd4c2e
saoncy <saooncy@gmail.com>
2023-02-22 19:00:42 +0200

    added code from lesson 2.3

---
+++ JavaScript/MATH/operators.js
@@ -0,0 +1,20 @@
+let x = 10 + 5;
+let y = 10 - 5;
+let z = 10 * 5;
+let q = 10 / 5;
+let w = 10 % 3;
+
+let complex = 10 + 5 * 10 - 8 / 2;
+let complex2 = (10 + 5) * (10 - 8) / 2;
+
+let a = 10;
+a++; // inc
+a--; // dec
+++a; // prefix inc
+--a; // prefix dec
+
+x += 5;
+y -= 5;
+z *= 5;
+q /= 5;
+w %= 3;
