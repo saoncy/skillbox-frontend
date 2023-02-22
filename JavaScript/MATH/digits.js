770a5802e1dae8f6bb2fff1995c503ffb357a077
saoncy <saooncy@gmail.com>
2023-02-22 18:54:14 +0200

    added code from leson 2.2

---
+++ JavaScript/MATH/digits.js
@@ -0,0 +1,13 @@
+let digit = -25;
+let fract = -25.03;
+let exp = -2e10; // 20000000000
+let expSmall = -2e-3; // 0.002
+let expFract = -1.25e3; // -1250
+let expFractSmall = -1.25e-3; // 0.00125
+let fract1 = -25.;
+let fract2 = -.25;
+
+let dec = 9;
+let bin = 0b1001;
+let oct = 0o10;
+let hex = 0x1b; // 0x1B
