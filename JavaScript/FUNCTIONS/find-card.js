831be57258bb57b730a966cfb73ba22b4edcfbc1
saoncy <saooncy@gmail.com>
2023-02-28 16:06:59 +0200

    added changes after lesson 5.4

--- JavaScript/FUNCTIONS/find-card.js
+++ JavaScript/FUNCTIONS/find-card.js
@@ -1,18 +1,33 @@
-function findCard(cards, wantedCard = 'Туз') {
+function findCardIndex(cards, wantedCard = 'Туз') {
   console.log(`Ищем карту '${wantedCard}' в колоде.`)
 
-  let found = false;
-
-  for (let card of cards) {
+  for (let index in cards) {
+    let card = cards[index];
     console.log(`Из колоды вытянута карта '${card}'.`)
 
     if (card === wantedCard) {
-      found = true;
-      break;
+      console.log(`В колоде найдена карта '${wantedCard}'.`);
+      return +index;
     }
   }
 
-  console.log(found ?
-    `В колоде найдена карта '${wantedCard}'.` :
-    `В колоде нет карты '${wantedCard}'.`)
+  // console.log(found ?
+  //   `В колоде найдена карта '${wantedCard}'.` :
+  //   `В колоде нет карты '${wantedCard}'.`)
+
+  console.log(`В колоде нет карты '${wantedCard}'`)
+
+  return -1;
 }
+
+
+let myCards = ['2', 'Король', 'Туз', '5', '6', 'Король'];
+
+// findCard(myCards);
+// findCard(myCards, '5')
+
+
+let aceIndex = findCardIndex(myCards);
+console.log(aceIndex);
+let jackIndex = findCardIndex(myCards, 'Валет');
+console.log(jackIndex);
