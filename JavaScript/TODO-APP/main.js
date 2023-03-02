cbe06140e8402416b80f65770793659a46a33fad
saoncy <saooncy@gmail.com>
2023-03-02 15:20:54 +0200

    code from lesson 8.3 - added todo item functionality

--- JavaScript/TODO-APP/main.js
+++ JavaScript/TODO-APP/main.js
@@ -14,7 +14,7 @@
 
     form.classList.add('input-group', 'mb-3');
     input.classList.add('form-control');
-    input.placeholder = 'Введите название нового дела:';
+    input.placeholder = 'Введите название нового дела';
     buttonWrapper.classList.add('input-group-append');
     button.classList.add('btn', 'btn-primary');
     button.textContent = 'Добавить дело';
@@ -36,6 +36,33 @@
     return list;
   }
 
+  function createTodoItem(name) {
+    let item = document.createElement('li');
+
+    let buttonGroup = document.createElement('div');
+    let doneButton = document.createElement('button');
+    let deleteButton = document.createElement('button');
+
+    item.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-center');
+    item.textContent = name;
+
+    buttonGroup.classList.add('button-group', 'button-group-sm');
+    doneButton.classList.add('btn', 'btn-success');
+    doneButton.textContent = 'Готово';
+    deleteButton.classList.add('btn', 'btn-danger');
+    deleteButton.textContent = 'Удалить';
+
+    buttonGroup.append(doneButton);
+    buttonGroup.append(deleteButton);
+    item.append(buttonGroup);
+
+    return {
+      item,
+      doneButton,
+      deleteButton,
+    };
+  }
+
   document.addEventListener('DOMContentLoaded', () => {
     let container = document.getElementById('todo-app');
 
@@ -46,6 +73,30 @@
     container.append(title);
     container.append(todoItemForm.form);
     container.append(todoList);
+
+    todoItemForm.form.addEventListener('submit', ev => {
+      ev.preventDefault();
+
+      if (!todoItemForm.input.value) {
+        return;
+      }
+
+      let todoItem = createTodoItem(todoItemForm.input.value);
+
+      todoItem.doneButton.addEventListener('click', () => {
+        todoItem.item.classList.toggle('list-group-item-success');
+      });
+
+      todoItem.deleteButton.addEventListener('click', () => {
+        if (confirm('Вы уверены?')) {
+          todoItem.item.remove();
+        }
+      });
+
+      todoList.append(todoItem.item);
+
+      todoItemForm.input.value = '';
+    });
   });
 
 })();
