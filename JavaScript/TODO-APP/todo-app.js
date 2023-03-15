f7c06fd49674baeab12ab2083a193abf4d858e29
saoncy <saooncy@gmail.com>
2023-03-15 14:32:55 +0200

    added POST request for adding new todo item

--- JavaScript/TODO-APP/todo-app.js
+++ JavaScript/TODO-APP/todo-app.js
@@ -58,26 +58,26 @@
 
     doneButton.addEventListener('click', () => {
       todoItem.classList.toggle('list-group-item-success');
-      let localStorageItems = JSON.parse(localStorage.getItem(key));
-      localStorageItems.forEach(el => {
-        if (el.name === item.name) {
-          el.done = !el.done;
-          localStorage.setItem(key, JSON.stringify(localStorageItems));
-        }
-      })
+      // let localStorageItems = JSON.parse(localStorage.getItem(key));
+      // localStorageItems.forEach(el => {
+      //   if (el.name === item.name) {
+      //     el.done = !el.done;
+      //     localStorage.setItem(key, JSON.stringify(localStorageItems));
+      //   }
+      // })
     });
 
     deleteButton.addEventListener('click', () => {
       if (confirm('Вы уверены?')) {
         todoItem.remove();
-        let localStorageItems = JSON.parse(localStorage.getItem(key));
-        localStorageItems.forEach((el, ind, arr) => {
-          if (el.name === item.name) {
-            if (arr.length === 1) arr = [];
-            arr.splice(ind, ind)
-            localStorage.setItem(key, JSON.stringify(arr));
-          }
-        })
+        // let localStorageItems = JSON.parse(localStorage.getItem(key));
+        // localStorageItems.forEach((el, ind, arr) => {
+        //   if (el.name === item.name) {
+        //     if (arr.length === 1) arr = [];
+        //     arr.splice(ind, ind)
+        //     localStorage.setItem(key, JSON.stringify(arr));
+        //   }
+        // })
       }
     });
 
@@ -85,11 +85,7 @@
     buttonGroup.append(deleteButton);
     todoItem.append(buttonGroup);
 
-    return {
-      todoItem,
-      doneButton,
-      deleteButton,
-    };
+    return todoItem;
   }
 
   function createTodoApp(container, key, title = 'Список дел', todoItems = []) {
@@ -115,32 +111,44 @@
       });
     }
 
-    let localStorageItems = localStorage.getItem(key);
-    if (localStorageItems) {
-      localStorageItems = JSON.parse(localStorageItems);
-      localStorageItems.forEach(item => {
-        todoList.append(createTodoItem(item, key).todoItem);
-      })
-    }
+    // let localStorageItems = localStorage.getItem(key);
+    // if (localStorageItems) {
+    //   localStorageItems = JSON.parse(localStorageItems);
+    //   localStorageItems.forEach(item => {
+    //     todoList.append(createTodoItem(item, key).todoItem);
+    //   })
+    // }
 
-    todoItemForm.form.addEventListener('submit', ev => {
+    todoItemForm.form.addEventListener('submit', async ev => {
       ev.preventDefault();
 
       if (!todoItemForm.input.value) {
         return;
       }
 
-      let todoListItem = {name: todoItemForm.input.value, done: false};
+      const response = await fetch('http://localhost:3000/api/todos', {
+        method: 'POST',
+        body: JSON.stringify({
+          name: todoItemForm.input.value.trim(),
+          owner: 'Saoncy',
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      });
 
-      if (!localStorage.getItem(key)) {
-        localStorage.setItem(key, JSON.stringify([todoListItem]));
-      } else {
-        let todoListArray = JSON.parse(localStorage.getItem(key));
-        todoListArray.push(todoListItem);
-        localStorage.setItem(key, JSON.stringify(todoListArray));
-      }
 
-      todoList.append(createTodoItem(todoListItem, key).todoItem);
+      let todoListItem = await response.json();
+
+      // if (!localStorage.getItem(key)) {
+      //   localStorage.setItem(key, JSON.stringify([todoListItem]));
+      // } else {
+      //   let todoListArray = JSON.parse(localStorage.getItem(key));
+      //   todoListArray.push(todoListItem);
+      //   localStorage.setItem(key, JSON.stringify(todoListArray));
+      // }
+
+      todoList.append(createTodoItem(todoListItem, key));
 
       todoItemForm.input.value = '';
       todoItemForm.button.disabled = true;
