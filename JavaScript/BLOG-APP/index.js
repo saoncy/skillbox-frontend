dfa314d40e82005021861dfe903c59cb2523bc7f
saoncy <saooncy@gmail.com>
2023-03-16 17:20:11 +0200

    fixed codestyle

--- JavaScript/BLOG-APP/index.js
+++ JavaScript/BLOG-APP/index.js
@@ -1,28 +1,27 @@
 (() => {
-
   function createBlogPostsList() {
-    const list = document.createElement('div');
+    const list = document.createElement("div");
 
-    list.classList.add('list-group', 'mx-auto', 'pt-5', 'col-6');
+    list.classList.add("list-group", "mx-auto", "pt-5", "col-6");
 
     return list;
   }
 
   async function createBlogPostsListItem(blogItem) {
-    const link = document.createElement('a');
-    const headerWrapper = document.createElement('div');
-    const header = document.createElement('h5');
-    const author = document.createElement('small');
-    const content = document.createElement('p');
+    const link = document.createElement("a");
+    const headerWrapper = document.createElement("div");
+    const header = document.createElement("h5");
+    const author = document.createElement("small");
+    const content = document.createElement("p");
 
-    link.classList.add('list-group-item', 'p-3', 'list-group-item-action');
-    link.href = '#';
-    link.setAttribute('id', blogItem.id);
-    headerWrapper.classList.add('w-100', 'd-flex', 'justify-content-between');
-    header.classList.add('mb-1', 'col-5', 'text-truncate');
+    link.classList.add("list-group-item", "p-3", "list-group-item-action");
+    link.href = "#";
+    link.setAttribute("id", blogItem.id);
+    headerWrapper.classList.add("w-100", "d-flex", "justify-content-between");
+    header.classList.add("mb-1", "col-5", "text-truncate");
     header.textContent = blogItem.title;
-    author.textContent = await getBlogPostAuthorName(blogItem['user_id']);
-    content.classList.add('mb-1', 'col-10', 'text-truncate');
+    author.textContent = await getBlogPostAuthorName(blogItem["user_id"]);
+    content.classList.add("mb-1", "col-10", "text-truncate");
     content.textContent = blogItem.body;
 
     headerWrapper.append(header);
@@ -34,11 +33,11 @@
   }
 
   function createPaginationButtonGroup(prev, next) {
-    const pagination = document.createElement('ul');
-    const prevButton = createPaginationButton(prev, 'Previous');
-    const nextButton = createPaginationButton(next, 'Next');
+    const pagination = document.createElement("ul");
+    const prevButton = createPaginationButton(prev, "Previous");
+    const nextButton = createPaginationButton(next, "Next");
 
-    pagination.classList.add('pagination', 'mt-2', 'justify-content-center');
+    pagination.classList.add("pagination", "mt-2", "justify-content-center");
 
     pagination.append(prevButton);
     pagination.append(nextButton);
@@ -47,11 +46,11 @@
   }
 
   function createPaginationButton(link, text) {
-    const buttonWrapper = document.createElement('li');
-    const pageLink = document.createElement('a');
+    const buttonWrapper = document.createElement("li");
+    const pageLink = document.createElement("a");
 
-    buttonWrapper.classList.add('page-item', 'col-3', 'text-center');
-    pageLink.classList.add('page-link');
+    buttonWrapper.classList.add("page-item", "col-3", "text-center");
+    pageLink.classList.add("page-link");
     pageLink.href = `index.html?page=${link}`;
     pageLink.textContent = `${text} page`;
 
@@ -60,45 +59,40 @@
     return buttonWrapper;
   }
 
-
   async function createBlogApp() {
-    const app = document.getElementById('blog-app');
+    const app = document.getElementById("blog-app");
     const blogList = createBlogPostsList();
     const pageParams = new URLSearchParams(window.location.search);
-    const pageNumber = pageParams.get('page') ? parseInt(pageParams.get('page')) : 1;
+    const pageNumber = pageParams.get("page")
+      ? parseInt(pageParams.get("page"))
+      : 1;
     const posts = await getBlogPostsList(pageNumber);
-    const pagination = createPaginationButtonGroup(pageNumber <= 1 ? 1 : pageNumber - 1, pageNumber + 1);
+    const pagination = createPaginationButtonGroup(
+      pageNumber <= 1 ? 1 : pageNumber - 1,
+      pageNumber + 1
+    );
+
     posts.forEach(async (post) => {
       const blogListItem = await createBlogPostsListItem(post);
       blogList.append(blogListItem);
     });
-  //   blogList.append(await createBlogPostsListItem({
-  //   "id": 2177,
-  //   "user_id": 78844,
-  //   "title": "Tenax defigo angulus synagoga qui video supplanto cohaero tubineus suadeo timor omnis sophismata deporto comedo vitae stella.",
-  //   "body": "Vulariter sustineo ciminatio. Accipio video fuga. Eaque pel vociferor. Curvo vesica arguo. Teres animus corroboro. Voluptatem copia custodia. Amet unde nisi. Bonus aut autem. In thalassinus ager. Arto valeo omnis. Sum adimpleo ab. Dolores delego demulceo. Sufficio adsum porro. Thesis officiis qui. Aptus turpe ipsam. Cupiditas conservo tempora. Aveho civitas appono. Debilito coruscus laudantium. Amet umbra defluo. Theatrum custodia contra. Patior degenero assumenda."
-  // }));
-  //   blogList.append(await createBlogPostsListItem({
-  //   "id": 2177,
-  //   "user_id": 78847,
-  //   "title": "Tenax defigo angulus synagoga qui video supplanto cohaero tubineus suadeo timor omnis sophismata deporto comedo vitae stella.",
-  //   "body": "Vulariter sustineo ciminatio. Accipio video fuga. Eaque pel vociferor. Curvo vesica arguo. Teres animus corroboro. Voluptatem copia custodia. Amet unde nisi. Bonus aut autem. In thalassinus ager. Arto valeo omnis. Sum adimpleo ab. Dolores delego demulceo. Sufficio adsum porro. Thesis officiis qui. Aptus turpe ipsam. Cupiditas conservo tempora. Aveho civitas appono. Debilito coruscus laudantium. Amet umbra defluo. Theatrum custodia contra. Patior degenero assumenda."
-  // }));
-  //   blogList.append(await createBlogPostsListItem({
-  //   "id": 2177,
-  //   "user_id": 78732,
-  //   "title": "Tenax defigo angulus synagoga qui video supplanto cohaero tubineus suadeo timor omnis sophismata deporto comedo vitae stella.",
-  //   "body": "Vulariter sustineo ciminatio. Accipio video fuga. Eaque pel vociferor. Curvo vesica arguo. Teres animus corroboro. Voluptatem copia custodia. Amet unde nisi. Bonus aut autem. In thalassinus ager. Arto valeo omnis. Sum adimpleo ab. Dolores delego demulceo. Sufficio adsum porro. Thesis officiis qui. Aptus turpe ipsam. Cupiditas conservo tempora. Aveho civitas appono. Debilito coruscus laudantium. Amet umbra defluo. Theatrum custodia contra. Patior degenero assumenda."
-  // }));
 
     app.append(blogList);
     app.append(pagination);
   }
 
   async function getBlogPostAuthorName(authorID) {
-    const response = await fetch(`https://gorest.co.in/public/v2/users/${authorID}`, {mode:'cors'});
-    if (!response.ok)
-      return 'Anonymous';
+    const response = await fetch(
+      `https://gorest.co.in/public/v2/users/${authorID}`,
+      {
+        mode: "cors",
+        headers: {
+          Authorization:
+            "Bearer *insert your key here*",
+        },
+      }
+    );
+    if (!response.ok) return "Anonymous";
 
     const authorObject = await response.json();
 
@@ -107,7 +101,16 @@
 
   async function getBlogPostsList(page) {
     if (!page || page < 0) page = 1;
-    const response = await fetch(`https://gorest.co.in/public/v2/posts?page=${page}`, {mode:'cors', headers: { Authorization: 'Bearer *insert your key here*'}});
+    const response = await fetch(
+      `https://gorest.co.in/public/v2/posts?page=${page}`,
+      {
+        mode: "cors",
+        headers: {
+          Authorization:
+            "Bearer *insert your key here*",
+        },
+      }
+    );
     const postsList = await response.json();
 
     return postsList;
