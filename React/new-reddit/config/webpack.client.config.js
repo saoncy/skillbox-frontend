a3d58b98e32f4aa585038e4dfd7714e9e1f83bc9
saoncy <saooncy@gmail.com>
2023-03-20 22:52:45 +0200

    added style modules + SASS preprocessor

--- React/new-reddit/config/webpack.client.config.js
+++ React/new-reddit/config/webpack.client.config.js
@@ -1,6 +1,6 @@
 const path = require("path");
-const { HotModuleReplacementPlugin } = require('webpack');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const { HotModuleReplacementPlugin } = require("webpack");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 const NODE_ENV = process.env.NODE_ENV;
 const IS_DEV = NODE_ENV === "development";
@@ -15,17 +15,18 @@ module.exports = {
   resolve: {
     extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
     alias: {
-      'react-dom': IS_DEV ? '@hot-loader/react-dom' : 'react-dom',
-    }
+      "react-dom": IS_DEV ? "@hot-loader/react-dom" : "react-dom",
+    },
   },
   mode: NODE_ENV ? NODE_ENV : "development",
-  entry: [path.resolve(__dirname, "../src/client/index.jsx"),
-          'webpack-hot-middleware/client?path=http://localhost:3001/static/__webpack_hmr'
+  entry: [
+    path.resolve(__dirname, "../src/client/index.jsx"),
+    "webpack-hot-middleware/client?path=http://localhost:3001/static/__webpack_hmr",
   ],
   output: {
     path: path.resolve(__dirname, "../dist/client"),
     filename: "client.js",
-    publicPath: '/static/'
+    publicPath: "/static/",
   },
   module: {
     rules: [
@@ -33,12 +34,26 @@ module.exports = {
         test: /\.[tj]sx?$/,
         use: ["ts-loader"],
       },
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: {
+                mode: "local",
+                localIdentName: "[name]__[local]--[hash:base64:5]",
+              },
+            },
+          },
+          "sass-loader"
+        ],
+      },
     ],
   },
   devtool: setupDevtool(),
-  plugins: IS_DEV ? [
-    new HotModuleReplacementPlugin(),
-    new CleanWebpackPlugin(),
-  ]
-  : [],
+  plugins: IS_DEV
+    ? [new HotModuleReplacementPlugin(), new CleanWebpackPlugin()]
+    : [],
 };
--- React/new-reddit/src/shared/Header.jsx
+++ React/new-reddit/src/shared/Header.jsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
 import { hot } from 'react-hot-loader/root'
+import styles from './Header.scss'
 
 
 function HeaderComponent() {
   return (
     <header>
-      <h1>Reddit for our own</h1>
+      <h1 className={styles.example}>Reddit for our own</h1>
     </header>
   )
 }
