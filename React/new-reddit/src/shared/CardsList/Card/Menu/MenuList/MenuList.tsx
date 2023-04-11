39f0d6335348070c9c702bcae698039d250d3420
saoncy <saooncy@gmail.com>
2023-04-11 20:05:40 +0300

    added the rest of icons

--- React/new-reddit/src/shared/CardsList/Card/Menu/MenuList/MenuList.tsx
+++ React/new-reddit/src/shared/CardsList/Card/Menu/MenuList/MenuList.tsx
@@ -4,14 +4,17 @@ import { GenericList } from '../../../../../utils/react/GenericList';
 import { generateID } from '../../../../../utils/js/generateRandomIndex';
 import { CommentsIcon } from '../../../../icons/CommentsIcon';
 import { ShareIcon } from '../../../../icons/ShareIcon';
+import { HideIcon } from '../../../../icons/HideIcon';
+import { SaveIcon } from '../../../../icons/SaveIcon';
+import { ReportIcon } from '../../../../icons/ReportIcon';
 
 export function MenuList() {
   const menuItems = [
     {value: 'Комментарии', className: styles.listItem, icon: <CommentsIcon />},
     {value: 'Поделиться', className: styles.listItem, icon: <ShareIcon />},
-    {value: 'Скрыть', className: styles.listItem},
-    {value: 'Сохранить', className: styles.listItem},
-    {value: 'Пожаловаться', className: styles.listItem},
+    {value: 'Скрыть', className: styles.listItem, icon: <HideIcon />},
+    {value: 'Сохранить', className: styles.listItem, icon: <SaveIcon />},
+    {value: 'Пожаловаться', className: styles.listItem, icon: <ReportIcon />},
   ].map(generateID);
 
   return (
